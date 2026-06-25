bf15c74f6f61c19c19e1adab20ac42c7edd045b7
swcidd <sanol.sherwinsid@gmail.com>
2026-06-25 14:01:56 +0800

    Added portfolio sections and adjusted hidden behavior; Adjusted to 'hidden' attribute from 'hidden' class

--- script.js
+++ script.js
@@ -2,15 +2,16 @@ const pressStartButton = document.getElementById("press-start-button");
 const pressStartScreen = document.getElementById("press-start-screen");
 const landingPage = document.getElementById("landing-page");
 
-landingPage.classList.add("hidden");
+pressStartScreen.hidden = false;
+landingPage.hidden = true;
 
 pressStartButton.addEventListener("click", () => {
-  pressStartScreen.classList.add("hidden");
-  landingPage.classList.remove("hidden");
+  pressStartScreen.hidden = true;
+  landingPage.hidden = false;
 });
 
 document.addEventListener("keydown", (event) => {
-  if (event.key === "Enter" && !pressStartScreen.classList.contains("hidden")) {
+  if (event.key === "Enter" && !pressStartScreen.hidden) {
     pressStartButton.click();
   }
 });
