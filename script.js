054a83f89c012f5fef11142de3f6d50e4f597dae
Xanth Reign Palmes <XanthPalmes@MacBook-Pro.local>
2026-06-27 20:40:18 +0800

    added triptych style intro

--- script.js
+++ script.js
@@ -1,47 +1,11 @@
-const pressStartButton = document.getElementById("press-start-button");
-let currentView = "pressStart";
+const startButton = document.getElementById("startButton");
+const overlay = document.getElementById("fade-overlay");
+const members = document.querySelector(".members");
 
-const views = {
-  pressStart: document.getElementById("press-start-screen"),
-  characterSelect: document.getElementById("character-select"),
-  documentation: document.getElementById("documentation"),
-  teamPortfolio: document.getElementById("team-portfolio"),
-  josephPortfolio: document.getElementById("joseph-portfolio"),
-  sherwinPortfolio: document.getElementById("sherwin-portfolio"),
-  xanthPortfolio: document.getElementById("xanth-portfolio"),
-};
+startButton.addEventListener("click", () => {
+  overlay.classList.add("active");
 
-function renderView(currentView) {
-  Object.entries(views).forEach(([name, element]) => {
-    element.hidden = name !== currentView;
-  });
-}
-
-/* PAGE SWITCHER */
-pressStartButton.addEventListener("click", () => {
-  renderView("characterSelect");
-});
-
-document.addEventListener("keydown", (event) => {
-  if (event.key === "Enter" && currentView === "pressStart") {
-    pressStartButton.click();
-  }
-});
-
-documentationButton.addEventListener("click", () => {
-  renderView("documentation");
-});
-
-teamPortfolioButton.addEventListener("click", () => {
-  renderView("teamPortfolio");
-});
-
-josephPortfolioButton.addEventListener("click", () => {
-  renderView("josephPortfolio");
-});
-sherwinPortfolioButton.addEventListener("click", () => {
-  renderView("sherwinPortfolio");
-});
-xanthPortfolioButton.addEventListener("click", () => {
-  renderView("xanthPortfolio");
+  setTimeout(() => {
+    members.classList.add("active");
+  }, 1000);
 });
